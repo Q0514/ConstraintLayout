f780e5cb7699d358a742f377a2be8f930bcea558
Q0514 <980382589@qq.com>
2021-07-09 15:41:30 +0800

    retrofit请求

---
+++ app/src/main/java/cn/study/uistudy/TestOkhttpActivity1.java
@@ -0,0 +1,56 @@
+package cn.study.uistudy;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.lang.annotation.Retention;
+
+import cn.study.uistudy.util.HttpBinService;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class TestOkhttpActivity1 extends AppCompatActivity {
+
+    private Retrofit retrofit;
+    private HttpBinService httpBinService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_okhttp1);
+        initRequest();
+        retrofit = new Retrofit.Builder().baseUrl("https://wanandroid.com/wenda/comments/14500/json").build();
+        httpBinService = retrofit.create(HttpBinService.class);
+    }
+    private void initRequest() {
+
+        retrofit2.Call<ResponseBody>call=httpBinService.get("lance","123");
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+                try {
+                    Log.i("111111","onResponse:"+response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/cn/study/uistudy/util/OkhttpHelper.java
+++ app/src/main/java/cn/study/uistudy/util/OkhttpHelper.java
@@ -1,8 +1,7 @@
 package cn.study.uistudy.util;
 
-import java.util.concurrent.Callable;
+
 import okhttp3.Callback;
-import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 public class OkhttpHelper {
